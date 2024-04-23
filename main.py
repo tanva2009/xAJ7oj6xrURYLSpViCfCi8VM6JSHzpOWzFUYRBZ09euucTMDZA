28da59edd38b08e845810c371962cfb547d74cdc
tanva2009 <137403914+tanva2009@users.noreply.github.com>
2024-04-24 01:55:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ import time
 
 currentVersion = "0.0.1"
 request_url = urllib.request.urlopen('https://raw.githubusercontent.com/tanva2009/xAJ7oj6xrURYLSpViCfCi8VM6JSHzpOWzFUYRBZ09euucTMDZA/main/Version.html') 
-encoding = 'utf-8'
-NewVersion = request_url.read().decode(encoding)
+NewVersion = request_url.read().decode('utf-8').strip()
 if (NewVersion == currentVersion):
     print("App is up to date!")
 else:
     newVersion = requests.get("https://raw.githubusercontent.com/tanva2009/xAJ7oj6xrURYLSpViCfCi8VM6JSHzpOWzFUYRBZ09euucTMDZA/main/main.py")
-    open("TAutoUpdate.py", "wb").write(newVersion.content)
+    open(__file__, "wb").write(newVersion.content)
     print("New version downloaded, restarting in 5 seconds!")
     time.sleep(5)
     quit()
