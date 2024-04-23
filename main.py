30c50c61b1e24363ee040d65ca26f66bbcdfeade
tanva2009 <137403914+tanva2009@users.noreply.github.com>
2024-04-24 01:41:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import urllib.request
 import requests
 import time
 
-currentVersion = "0.0.2"
-request_url = urllib.request.urlopen('https://raw.githubusercontent.com/tanva2009/xAJ7oj6xrURYLSpViCfCi8VM6JSHzpOWzFUYRBZ09euucTMDZA/main/Version.html?token=GHSAT0AAAAAACQZZ6JD7OOY6UVEXMQEELECZRH7R7Q') 
-if (request_url.read() == currentVersion):
+currentVersion = "0.0.1"
+request_url = urllib.request.urlopen('https://raw.githubusercontent.com/tanva2009/xAJ7oj6xrURYLSpViCfCi8VM6JSHzpOWzFUYRBZ09euucTMDZA/main/Version.html') 
+encoding = 'utf-8'
+NewVersion = request_url.read().decode(encoding)
+if (NewVersion == currentVersion):
     print("App is up to date!")
 else:
-    newVersion = requests.get("https://raw.githubusercontent.com/tanva2009/xAJ7oj6xrURYLSpViCfCi8VM6JSHzpOWzFUYRBZ09euucTMDZA/main/main.py?token=GHSAT0AAAAAACQZZ6JCSKPNLQEDKAPDZXPMZRH7SIA")
+    newVersion = requests.get("https://raw.githubusercontent.com/tanva2009/xAJ7oj6xrURYLSpViCfCi8VM6JSHzpOWzFUYRBZ09euucTMDZA/main/main.py")
     open("TAutoUpdate.py", "wb").write(newVersion.content)
     print("New version downloaded, restarting in 5 seconds!")
     time.sleep(5)
